590556efe22d720e722674323c0035a0982d2abb
illialaz <illialaziuk2@gmail.com>
2022-06-13 03:14:12 +0300

    Change structure of handler

--- products-service/handler.js
+++ products-service/handler.js
@@ -3,10 +3,24 @@
 module.exports.getProducts = async (event) => {
   return {
     statusCode: 200,
-    body: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: [
+      {
+        id: '1',
         brand: 'Porsche',
         model: '944s2',
-        engineCapacity: 3.0
+        engineCapacity: 3.0,
+        price: 4000
+      },
+      {
+        id: '2',
+        brand: 'Porsche',
+        model: '911',
+        engineCapacity: 3.4,
+        price: 20000
       }
+    ]
   };
 };
